22dbd4c6acbfe3007e82877fbeb9f0cf95214050
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2014-09-26 14:43:55 +0200

    listallwords.sh: works wonders

--- listallwords.sh
+++ listallwords.sh
@@ -2,4 +2,38 @@
 #
 # lists all words of every tex file in this folder
 
-cat *.tex | sed -e 's/%.*$//' -e 's/\\[a-z]\+/ /g' -e 's/\[[^]]*\]/ /g' -e 's/[{][a-z_0-9:-]*[}]/ /g' -e 's/[^a-zA-Z_-:]/ /g' -e 's/\([a-z]\)[,.?:]\(\s\|$\)/\1 /g' | xargs -n1 | grep -v '^.$' | grep -v '_$' | grep -v '^_' | sort -u
+{
+    #######################
+    # extract every word  #
+    ######################################################################
+    # 1. remove LaTeX comments                                           #
+    # 2. remove LaTeX commands                                           #
+    # 3. remove LaTeX command options                                    #
+    # 4. remove LaTeX command parameters, if they are no text (captions) #
+    # 5. remove '\-' hyphenation marks                                   #
+    # 6. remove everything that's no word character                      #
+    # 7. remove punctuation marks                                        #
+    # 8. remove hyphens                                                  #
+    ######################################################################
+    sed -e 's/%.*$//' -e 's/\\[a-zA-Z]\+/ /g' -e 's/\[[^]]*\]/ /g' -e 's/[{][a-z_0-9:-]*[}]/ /g' -e 's/\\-//g' -e 's/[^a-zA-Z_-:]/ /g' -e 's/\([a-zA-Z-]\)[,.?:]\(\s\|$\)/\1 /g' -e 's/-/ /g' *.tex
+} | {
+    #####################
+    # one word per line #
+    #####################
+    xargs -n1 | sort -u
+} | {
+    #########################################
+    # remove case-insensitive-similar words #
+    #########################################
+    uniq -i
+} | {
+    #####################################################
+    # remove lines up to one character or an underscore #
+    #####################################################
+    grep -Pv '^.?$|^_|_$'
+} | {
+    ###############################################
+    # strip different, but similar german endings #
+    ###############################################
+    xargs | sed -r 's/((\S+?)[et]?[nrs]?)( \2[et]?[nrs]?)+(\s|$)/\1 \4/g' | xargs -n1
+}

e8bbe29087e8ebc139b855759d4b9f4e8070198c
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2014-10-16 19:52:17 +0200

    showmishaps.sh and stuff

--- showmishaps.sh
+++ showmishaps.sh
@@ -3,7 +3,11 @@
 # shows all minor latex mishaps for all .tex files
 
 listfiles(){
-    find -name '*.tex'
+    find * -name '*.tex'
+}
+
+listlogs(){
+    find * -name '*.log'
 }
 
 findmatch='listfiles | xargs grep -n'
@@ -23,5 +27,14 @@ echo
 
 echo "<= dots within lines (multiple sentences) =>"
 echo
-{ listfiles | xargs grep -Pn '^[^%&]*(?<!engl|Kap|S|Abb|Tab|Gl|Anh|Ref|Prof|vs|Dr|E|z\.B|et al|unters|ca|eam)\.(?!$|[0-9]|pdf|com|eam|\s*(\\todo|%|\\\\|,|\})|Sc\.|B\.)' || echo "ok"; } | grep -Pv '\\(If|State)|@'
+listfiles | xargs grep -Pn '^[^%&]*(?<!engl|Kap|S|Abb|Tab|Gl|Anh|Ref|Prof|vs|Dr|z\.B|et al|unters|ca|eam)\.(?!$|[0-9]|pdf|com|eam|\s*(\\todo|%|\\\\|,|\})|Sc\.|B\.)' | grep -Pv '\\(If|State)|\\dcauthoremail|Stefan E\. Schulz' || echo "ok"
+echo
 
+###########################################
+# undefined or multiple labels/references #
+###########################################
+
+echo "<= undefined references and  multiple labels =>"
+echo
+listlogs | { xargs grep -Pi 'multipl[ey]|undefined' || echo "ok";} | sed -r -n "s/^[^\`]*\`([^']+)'.*$/\1/p"
+echo

3c68f72c1e08fd7c194d8eeb367fb6efc32c5b9c
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2014-10-21 16:54:55 +0200

    progress

--- showmishaps.sh
+++ showmishaps.sh
@@ -27,7 +27,7 @@ echo
 
 echo "<= dots within lines (multiple sentences) =>"
 echo
-listfiles | xargs grep -Pn '^[^%&]*(?<!engl|Kap|S|Abb|Tab|Gl|Anh|Ref|Prof|vs|Dr|z\.B|et al|unters|ca|eam)\.(?!$|[0-9]|pdf|com|eam|\s*(\\todo|%|\\\\|,|\})|Sc\.|B\.)' | grep -Pv '\\(If|State)|\\dcauthoremail|Stefan E\. Schulz' || echo "ok"
+listfiles | xargs grep -Pn '^[^%&]*(?<!engl|Kap|S|Abb|Tab|Gl|Anh|Ref|Prof|vs|Dr|z\.B|et al|unters|ca|eam)\.(?!$|[0-9]|pdf|cpp|com|eam|\s*(\\todo|%|\\\\|,|\})|Sc\.|B\.)' | grep -Pv '\\(If|State)|\\dcauthoremail|Stefan E\. Schulz' || echo "ok"
 echo
 
 ###########################################

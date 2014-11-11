9e753f03b6d6111dd062d6311804f5e386854e69
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2014-11-11 11:47:57 +0100

    extracting every single formula, whatever the cost

--- drawequations.sh
+++ drawequations.sh
@@ -2,29 +2,44 @@
 #
 # from the extracted equations, takes every single one and draws it
 
+rm -rf equations/each/*
+
+ncpus=`grep proc /proc/cpuinfo | wc -l`
+[ -z "$ncpus" ] && ncpus=8
+
+checksums=""
 index=1
 createfile(){
     mkdir -p equations/each
-    (
-        cd equations/each
-        cat <<EOF > $index.tex
+    pushd equations/each || return
+    cat <<EOF | sed -r 's/(begin|end)\{(equation|gather|align)/&*/' >  $index.tex
 \documentclass{scrreprt}
 
 \usepackage{amsmath}
 \usepackage{amsfonts}
 \usepackage[range-phrase={\,bis\,},binary-units,retain-explicit-plus]{siunitx}
+\usepackage[paperwidth=200pt,paperheight=100pt]{geometry}
 
 \begin{document}
+
+\pagenumbering{gobble}
 
 $1
 
 \end{document}
 
 EOF
-        pdflatex $index.tex
-        pdf2png.sh $index.pdf
-    )
 
+    local sum=`cat $index.tex | md5sum`
+    if ! grep -q $sum <<< "$checksums"; then
+        checksums="$checksums
+$sum"
+        
+        while (( `jobs -rp | wc -l` >= $ncpus )) ; do sleep 0.1; done
+        { pdflatex $index.tex && pdf2png.sh $index.pdf; } &
+        
+    fi
+    popd
     let index++
 }
 
@@ -34,9 +49,8 @@ cat equations/equations.tex | while IFS= read -r line; do
     if [ -z "$equation" ]; then
         if grep -qP '\\begin\{(equation|gather|align)\}' <<< "$line"; then
             equation="$line"
-            echo "$line" does match
-#        else
-#            echo "$line" doesnt match
+            #        else
+            #            echo "$line" doesnt match
         fi
     else
         equation="$equation
@@ -48,3 +62,7 @@ $line"
         fi        
     fi
 done
+
+wait
+
+echo "done"

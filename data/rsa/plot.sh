704072779062885b4fbe4773afba437f1f60078d
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2014-11-25 20:40:38 +0100

    colortbl somewhat fixed

--- data/rsa/plot.sh
+++ data/rsa/plot.sh
@@ -2,14 +2,19 @@
 #
 # grabs the results from the percolation files and 
 
+mean='Mittelwert'
+max='Maximum'
+
 for f in *txt ; do
     [ -s "$f" ] || continue
     echo -n "$f "|grep -o '[0-9]\+'
-    awk '{a+=$3} END {print a/NR}' $f
-done | xargs -n2 | sort -n > results.txt
+    grep -v '#' $f | awk '{a+=$3; if ($3>b) {b=$3}} END {print a/NR,b}'
+done | xargs -n3 | sort -n | column -t > results.txt
 
-dataplot.py -f --xlog results.txt
+awk '{print $1,$2}' results.txt | column -t > "$mean"
+awk '{print $1,$3}' results.txt | column -t > "$max"
 
-mv -v plot.pdf maxdensity.pdf
-mv -v plot.png maxdensity.png
+dataplot.py --yrange 0:0.32 -f --xlabel 'Versuche' --ylabel 'Dichte' --xlog "$max" "$mean" || exit 1
 
+mv -v plot.pdf rsa_maxdensity.pdf
+mv -v plot.png rsa_maxdensity.png
